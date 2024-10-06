10e108fcd1b55fef7a470858879f1060cb8879e0
seahyael <yael.kwon@gmail.com>
2024-10-06 23:16:48 +0900

    photo edit

--- src/components/Pages/Homepage.js
+++ src/components/Pages/Homepage.js
@@ -26,7 +26,7 @@ const Homepage = () => {
         <div className="homepage-inner">
           <div className="homepage-item active">
             <img
-              src={'./CRUD.webp'}
+              src="./CRUD.png"
               alt="Los Angeles"
               className="d-block"
             />
